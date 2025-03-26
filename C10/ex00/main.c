8b7bdaf30ffccc6a62191ae0390131ef5fef0705
Loan <loan@buchs.fr>
2025-03-26 20:46:33 +0100

    suite

---
+++ C10/ex00/main.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loan <loan@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 17:48:55 by loan              #+#    #+#             */
+/*   Updated: 2025/03/26 19:32:44 by loan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int	ft_display_file(char *file_name)
+{
+	int		file;
+	int		reading;
+	char	filecontent;
+
+	file = open(file_name, 0);
+	if(file == -1)
+		return(0);
+	while ((reading = read(file, &filecontent, 1)))
+	{
+		if (reading == -1)
+			return (0);
+		write(1, &filecontent, 1);
+	}
+	close(file);
+	return (1);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		if (!ft_display_file(argv[1]))
+			write(1 ,"Cannot read file.", 17);
+	}
+	else
+		if (argc < 2)
+			write(1 ,"File name missing.", 18);
+		else
+			write(1 ,"Too many arguments.", 19);
+	return 0;
+}
