8e84b5c85f95beb08d449728512f46237afccb0e
Loan <loan@buchs.fr>
2025-04-27 23:09:00 +0200

    suite encore

--- C10/ex00/main.c
+++ C10/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: loan <loan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 17:48:55 by loan              #+#    #+#             */
-/*   Updated: 2025/03/26 19:32:44 by loan             ###   ########.fr       */
+/*   Updated: 2025/04/27 23:04:22 by loan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,33 @@ int	ft_display_file(char *file_name)
 	char	filecontent;
 
 	file = open(file_name, 0);
-	if(file == -1)
-		return(0);
-	while ((reading = read(file, &filecontent, 1)))
+	if (file == -1)
+		return (0);
+	reading = read(file, &filecontent, 1);
+	while (reading)
 	{
 		if (reading == -1)
 			return (0);
 		write(1, &filecontent, 1);
+		reading = read(file, &filecontent, 1);
 	}
 	close(file);
 	return (1);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
 		if (!ft_display_file(argv[1]))
-			write(1 ,"Cannot read file.", 17);
+			write(1, "Cannot read file.", 17);
 	}
 	else
+	{
 		if (argc < 2)
-			write(1 ,"File name missing.", 18);
+			write(1, "File name missing.", 18);
 		else
-			write(1 ,"Too many arguments.", 19);
-	return 0;
+			write(1, "Too many arguments.", 19);
+	}
+	return (0);
 }
--- C10/ex01/main.c
+++ C10/ex01/main.c
@@ -6,12 +6,70 @@
 /*   By: loan <loan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 20:29:10 by loan              #+#    #+#             */
-/*   Updated: 2025/04/14 19:29:12 by loan             ###   ########.fr       */
+/*   Updated: 2025/04/27 23:02:13 by loan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char const *argv[])
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int	ft_display_file(char *file_name)
 {
-	/* code */
-	return 0;
+	int		file;
+	int		reading;
+	char	filecontent;
+
+	file = open(file_name, 0);
+	if (file == -1)
+		return (0);
+	reading = read(file, &filecontent, 1);
+	while (reading)
+	{
+		if (reading == -1)
+			return (0);
+		write(1, &filecontent, 1);
+		reading = read(file, &filecontent, 1);
+	}
+	close(file);
+	return (1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	int		a;
+	char	buffer[30000];
+
+	while (argc == 1)
+	{
+		a = read(0, buffer, 29999);
+		write(1, buffer, a);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		a = ft_display_file(argv[i]);
+		if (a == 0)
+		{
+			write(1, "ft_cat: ", 8);
+			write(1, argv[i], ft_strlen(argv[i]));
+			write(1, ": ", 2);
+			write(1, strerror(errno), ft_strlen(strerror(errno)));
+			write(1, "\n", 1);
+		}
+		i++;
+	}
+	return (0);
 }
